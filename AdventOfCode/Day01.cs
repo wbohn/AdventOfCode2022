7b05f9a21ab75726a7743e93743157991d9905a7
William Bohn <william.bohn@corsosystems.com>
2022-11-30 22:35:31 -0600

    add Solutions folder and Tests project

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -7,9 +7,18 @@ public class Day01 : BaseDay
     public Day01()
     {
         _input = File.ReadAllText(InputFilePath);
+        Console.WriteLine(_input);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {           
+        Console.WriteLine("Solve_1()");
+        return ValueTask.FromResult(_input);
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        Console.WriteLine("Solve_2()");
+        return ValueTask.FromResult(_input);
+    }
 }
