dc2a6abf938bcef5669883eda742d7542b581f57
William Bohn <william.bohn@corsosystems.com>
2022-12-02 00:11:35 -0600

    Day02

--- AdventOfCode/Days/Day02.cs
+++ AdventOfCode/Days/Day02.cs
@@ -2,19 +2,132 @@
 
 public class Day02 : AbstractBaseDay
 {
+    private readonly Dictionary<string, string> _choices = new()
+    {
+        {"A", "Rock" },
+        {"B", "Paper" },
+        {"C", "Scissors" },
+        {"X", "Rock" },
+        {"Y", "Paper" },
+        {"Z", "Scissors" }
+    };
+
+    private readonly Dictionary<string, string> _winningPlays = new()
+    {
+        {"Rock", "Paper" },
+        {"Paper", "Scissors" },
+        {"Scissors", "Rock" }
+    };
+
+    private readonly Dictionary<string, string> _losingPlays = new()
+    {
+        {"Paper", "Rock" },
+        {"Scissors", "Paper" },
+        {"Rock", "Scissors" }
+    };
+
+    private readonly Dictionary<string, int> _choiceScores = new()
+    {
+        {"Rock", 1 },
+        {"Paper", 2 },
+        {"Scissors", 3 }
+    };
+
+    private int _score = 0;
+    private int _score2 = 0;
+
     public Day02()
     {
     }
 
     public override ValueTask<string> Solve_1()
     {
-        var answer = "";
+        foreach (var match in _lines)
+        {
+            var matchChocies = match.Split(" ");
+            var opponent = _choices[matchChocies[0]];
+            var mine = _choices[matchChocies[1]];
+
+            _score += _choiceScores[mine];
+            _score += DoComparison(opponent, mine);
+
+        }
+        
+        var answer = _score.ToString();
         return ValueTask.FromResult(answer);
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var answer = "";
+        foreach (var match in _lines)
+        {
+            var matchChoices = match.Split(" ");
+            var opponentRaw = matchChoices[0];
+            var opponentText = _choices[opponentRaw];
+
+            var outcome = matchChoices[1];//_choices[matchChocies[1]];
+            var mineText = _choices[outcome];
+
+            var play = GetPlay(opponentText, outcome);
+
+
+            _score2 += GetChoiceScore(play);
+
+            _score2 += DoComparison2(outcome);
+        }
+
+        var answer = _score2.ToString();
         return ValueTask.FromResult(answer);
+    }
+
+    private int DoComparison(string opponentChoice, string myChoice)
+    {
+        if (opponentChoice == myChoice)
+        {
+            return 3; // draw
+        }
+
+        if (_winningPlays[myChoice] == opponentChoice)
+        {
+            return 0; // lose
+        } else
+        {
+            return 6; // win
+        }
+    }
+
+    private int DoComparison2(string outcome)
+    {
+        if (outcome == "X")
+        {
+            return 0; // lose
+        }
+
+        if (outcome == "Y")
+        {
+            return 3; // draw
+        }
+
+        return 6; // win
+    }
+
+    private int GetChoiceScore(string choiceText)
+    {
+        return _choiceScores[choiceText];
+    }
+
+    private string GetPlay(string opponentChoice, string outcome)
+    {
+        if (outcome == "X") // lose
+        {
+            return _losingPlays[opponentChoice];
+        }
+
+        if (outcome == "Y") // draw
+        {
+            return opponentChoice;
+        }
+
+        return _winningPlays[opponentChoice]; // win
     }
 }
